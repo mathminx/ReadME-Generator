e542a9da78480cd177a63b1690837bbc4e91de2d
mathminx <122234007+mathminx@users.noreply.github.com>
2023-03-19 15:58:46 -0300

    MVP minus license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+/*
+function renderLicenseBadge(license) {
+  if (license == '') {
+    return '';
+  }
+  return `![License: ](https://img.shields.io/badge/License-${license}-blueviolet.svg)]`
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  if (license == '') {
+    console.log("None");
+    return '';
+  }
+  console.log(license);
+  return `\n* [License](#license)\n`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that creates and returns the license section of the README
+// If there is no license, returns an empty string
+function renderLicenseSection(license) {
+  if (license == '') {
+    console.log('');
+    return '';
+  }
+  console.log(license);
+  return `## License
+    
+  Licensed under the ${license} license.
+  
+  `;
+}
+*/
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for the README file
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${data.license}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${data.license}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  If you have any questions, please contact me via email at ${data.email}.
+
+  Visit my GitHub page at [${data.githubUserName}](https://github.com/${data.githubUserName}/)!
 `;
 }
 
